73cda6771423a5a65d1b0be7b82524aa30ad4d59
AndresMardonesUACH <andymardones1@gmail.com>
2023-09-25 21:36:45 -0300

    pruebas añadidas

--- app/api/route.js
+++ app/api/route.js
@@ -4,11 +4,14 @@ export const tests =
     [
         {subjectId:"BAIN083", subjectName:"Cálculo En Varias Variables", testDate: "08 29, 2023 17:30:00 GMT -03:00"},
         {subjectId:"BAIN083", subjectName:"Cálculo En Varias Variables", testDate:"10 03, 2023 17:30:00 GMT -03:00"},
-        {subjectId:"INFO099", subjectName:"Estructura Discretas", testDate:"09 07, 2023 09:50:00 GMT -03:00"},
+        {subjectId:"INFO099", subjectName:"Estructura Discretas", testDate:"11 23, 2023 09:50:00 GMT -03:00"},
         {subjectId:"INFO099", subjectName:"Estructura Discretas", testDate:"10 17, 2023 09:50:00 GMT -03:00"},
         {subjectId:"BAIN087", subjectName:"Métodos Numéricos", testDate:"09 13, 2023 17:30:00 GMT -03:00"},
         {subjectId:"BAIN087", subjectName:"Métodos Numéricos", testDate:"10 25, 2023 17:30:00 GMT -03:00"},
         {subjectId:"BAIN085", subjectName:"Física: Ondas y Electromagnetismo", testDate:"10 18, 2023 17:30:00 GMT -03:00"},
+        {subjectId:"BAIN091", subjectName:"Probabilidad y estadística", testDate:"10 13, 2023 17:30:00 GMT -03:00"},
+        {subjectId:"BAIN091", subjectName:"Probabilidad y estadística", testDate:"11 10, 2023 17:30:00 GMT -03:00"},
+        {subjectId:"INFO090", subjectName:"Programación orientada a objetos", testDate:"10 19, 2023 09:50:00 GMT -03:00"},
     ]
 
 export async function GET() {
